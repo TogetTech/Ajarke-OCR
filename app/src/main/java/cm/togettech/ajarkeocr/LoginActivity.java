e5c8e6053a036a7c2a49ad03f7a34388b32345c4
loveline-so <lovelinesokou@gmail.com>
2021-02-03 13:48:05 +0100

    All init

--- app/src/main/java/cm/togettech/ajarkeocr/LoginActivity.java
+++ app/src/main/java/cm/togettech/ajarkeocr/LoginActivity.java
@@ -43,13 +43,66 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (emailTextInput.getText().toString().contentEquals("")) {
+
+
+                    errorView.setText("Email cant be empty");
+
+
+                } else if (passwordTextInput.getText().toString().contentEquals("")) {
+
+                    errorView.setText("Password cant be empty");
+
+                } else {
+
+
+                    mAuth.signInWithEmailAndPassword(emailTextInput.getText().toString(), passwordTextInput.getText().toString())
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        if (user != null) {
+
+                                            System.out.println("Email Verified : " + user.isEmailVerified());
+                                            Intent HomeActivity = new Intent(LoginActivity.this, MainActivity.class);
+                                            setResult(RESULT_OK, null);
+                                            startActivity(HomeActivity);
+                                            LoginActivity.this.finish();
+                                        }
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        if (task.getException() != null) {
+                                            errorView.setText(task.getException().getMessage());
+                                        }
+
+                                    }
+
+                                }
+                            });
+
+
+                }
+
+
             }
         });
+
+
+
+
     }
 }
